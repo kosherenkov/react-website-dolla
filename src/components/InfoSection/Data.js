1e0dffda83235c411502b0287109d0353d38511b
Artem <a.kosherenkov@gmail.com>
2020-10-24 23:29:51 +0300

    add info section

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,50 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headline: 'Unlimited Transactions with zero fees',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, vel in. Nam adipisci voluptatem placeat. Unde, ad    distinctio.',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: require('../../images/1.svg'),
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+};
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Unlimited Accesse',
+    headline: 'Login to your account at any time',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, vel in. Nam adipisci voluptatem placeat. Unde, ad    distinctio.',
+    buttonLabel: 'Lern More',
+    imgStart: true,
+    img: require('../../images/3.svg'),
+    alt: 'Piggybank',
+    dark: false,
+    primary: false,
+    darkText: true
+};
+
+export const homeObjThree = {
+    id: 'signup',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Join our team',
+    headline: 'Creating an account is extremely easy',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, vel in. Nam adipisci voluptatem placeat. Unde, ad    distinctio.',
+    buttonLabel: 'Start Now',
+    imgStart: false,
+    img: require('../../images/2.svg'),
+    alt: 'Paper',
+    dark: false,
+    primary: false,
+    darkText: true
+};
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -24,7 +24,7 @@ const Navbar = ({ toggle }) => {
               <NavLinks to="services">Services</NavLinks>
             </NavItem>
             <NavItem>
-              <NavLinks to="singup">Sing up</NavLinks>
+              <NavLinks to="signup">Sign up</NavLinks>
             </NavItem>
           </NavMenu>
           <NavBtn>
